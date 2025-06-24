f7237b00dfd14bc0b2d214e72ec7266135d07ed1
Anushka101010 <creativeanushka21@gmail.com>
2025-06-24 12:14:09 +0000

    basic structure

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-# print ("hello")
-# print ("in this program I will add two numbers")
-# a = "hello"
-# print (a)
-# even_num = "2"
-# print (even_num)
+print ("hello")
+print ("in this program I will add two numbers")
+a = "hello"
+print (a)
+even_num = "2"
+print (even_num)
 
 """datatypes
 1. text-type: string (str)
@@ -28,10 +28,85 @@ print (type(name))
 exp3 = 5*4
 print (type(exp3))
 
-a = 367
+# implicit typecasting
+a = 367 
 b = 364.8
 print (type(a))
 print (type(b))
 c = a/b
 print (c)
 print (type(c))
+
+# explicit typecasting
+a = "479"
+b = 54.66
+print (type(a))
+print (type(b))
+a = int(a)
+print ("after conversion",type(a))
+c = a+b
+print (c)
+print (type(c))
+
+# exercises practice
+# write a program to display a person's name, age and address in three different lines.
+name = "anushka"
+age = "22"
+address = "654 lake street"
+print (name)
+print (age)
+print (address)
+
+# write a program to swap two variables
+method 1
+x = 12
+y = 13
+temp = x
+print (temp)
+x = y
+print (x)
+y = temp
+print (y)
+print ("value of x is",x)
+print ("value of y is",y)
+
+# method 2
+a = 30
+b = 40
+# left,right = right,left
+a,b = b,a
+print (b)
+print (a)
+
+# write a program to convert float into integer
+a = 438.57
+print (type(a))
+a = int(a)
+print ("after conversion",type(a))
+print (a) 
+
+# write a program to take details from a student for ID-Card and then print it in different lines
+name = input("type your name here: ")
+grade = input("enter the grade of the student: ")
+age = int(input("enter the age of the student: "))
+email = input("enter the email of the student: ")
+ph_no = input ("enter the ph_no here: ")
+print ("Student Identity Card")
+print ("name: ",name)
+print ("grade: ",grade)
+print ("age: ",age)
+print ("email: ",email)
+print ("ph_no: ",ph_no)
+
+#write a program to take on user input as integer and then convert it to float.
+a = int(input("enter a number here: "))
+print (a)
+print (type(a))
+a = float(a)
+print ("after conversion",a)
+print (type(a))
+
+
+
+
+
